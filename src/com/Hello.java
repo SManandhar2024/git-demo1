c3f292f8e94ceac826a85dc6b0bc3527d7480640
SManandhar2024 <sunita_manandhar@live.com>
2025-04-30 23:18:00 -0400

    done

--- src/com/Hello.java
+++ src/com/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("this statement was written at 9:14 am");
-		System.out.println("this is second statement");
+	
 		System.out.println("Hello");
 
 	}
